b687cbe9c0f4621f85f532dfbf480dfc56ffd42c
Hardy Simpson <HardySimpson@gmail.com>
2011-12-29 10:35:43 +0800

    add COPYING

--- rule.c
+++ rule.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the Xlog Library.
+ *
+ * Copyright (C) 2011 by Hardy Simpson <HardySimpson@gmail.com>
+ *
+ * The Xlog Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Xlog Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Xlog Library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string.h>
 #include <ctype.h>
 #include <syslog.h>
--- zc_defs.h
+++ zc_defs.h
@@ -1,8 +1,20 @@
-/* $Id$
+/*
+ * This file is part of the Xlog Library.
  *
- * Copyright 2001-2003, Meiosys (www.meiosys.com). All rights reserved.
+ * Copyright (C) 2011 by Hardy Simpson <HardySimpson@gmail.com>
  *
- * See the COPYING file for the terms of usage and distribution.
+ * The Xlog Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Xlog Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Xlog Library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __zc_defs_h
