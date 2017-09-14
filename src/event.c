1ff8ed2ff4a92f59db4b20900ec694262b7635ec
chinarulezzz <s.alex08@mail.ru>
2017-09-14 17:17:08 +0300

    Fix memory leak: a_event
    	modified:   event.c

--- src/event.c
+++ src/event.c
@@ -60,6 +60,7 @@ zlog_event_t *zlog_event_new(int time_cache_count)
 	a_event->time_caches = calloc(time_cache_count, sizeof(zlog_time_cache_t));
 	if (!a_event->time_caches) {
 		zc_error("calloc fail, errno[%d]", errno);
+		free(a_event);
 		return NULL;
 	}
 	a_event->time_cache_count = time_cache_count;
