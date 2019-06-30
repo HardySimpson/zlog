fa8f433392d59b9163d32a92de7c34acf3b6d52b
manar <manar.zaboub@gmail.com>
2019-07-01 00:32:50 +0200

    Change name of confpath to config

--- src/conf.h
+++ src/conf.h
@@ -40,7 +40,7 @@ typedef struct zlog_conf_s {
 
 extern zlog_conf_t * zlog_env_conf;
 
-zlog_conf_t *zlog_conf_new(const char *confpath);
+zlog_conf_t *zlog_conf_new(const char *config);
 void zlog_conf_del(zlog_conf_t * a_conf);
 void zlog_conf_profile(zlog_conf_t * a_conf, int flag);
 
