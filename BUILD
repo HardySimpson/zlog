8579a9c9b1ffe661bc1d193cc43c884fc496862c
Anna Lito Michala <annalito.michala@wallee.com>
2023-10-27 13:40:26 +0200

    removed unseen characters that make the file incompatible with older gcc versions. Added a BUILD file for anyone interested in building with bazel

---
+++ BUILD
@@ -0,0 +1,25 @@
+load("@rules_cc//cc:defs.bzl", "cc_library")
+
+cc_library(
+    name = "zlog",
+    srcs = glob(
+        [
+            "src/*.c",
+            "src/*.h",
+        ],
+        exclude = [
+            "src/zlog_win.c",
+            "src/zlog_win.h",
+        ],
+    ),
+    hdrs = glob(
+        [
+            "src/*.h",
+            "*.h",
+        ],
+        exclude = [
+            "src/zlog_win.h",
+        ],
+    ),
+    visibility = ["//main:__pkg__"],
+)
--- src/lockfile.c
+++ src/lockfile.c
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * =============================================================================
  *
  * \file lockfile.c
@@ -14,47 +14,39 @@
 #include "lockfile.h"
 #include "zc_profile.h"
 
-LOCK_FD lock_file(char* path)
-{
-    if (!path || strlen(path) <= 0)
-    {
+LOCK_FD lock_file(char* path) {
+    if (!path || strlen(path) <= 0) {
         return INVALID_LOCK_FD;
     }
 #ifdef _WIN32
     LOCK_FD fd = CreateFile(path, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (fd == INVALID_LOCK_FD)
-    {
+    if (fd == INVALID_LOCK_FD) {
         DWORD err = GetLastError();
-		zc_error("lock file error : %d ", err);
+        zc_error("lock file error : %d ", err);
     }
 #else
     LOCK_FD fd = open(path, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
-    if (fd == INVALID_LOCK_FD)
-    {
-		zc_error("lock file error : %s ", strerror(errno));
+    if (fd == INVALID_LOCK_FD) {
+        zc_error("lock file error : %s ", strerror(errno));
     }
 #endif
     return fd;
 }
 
-bool unlock_file(LOCK_FD fd)
-{
-    if (fd == INVALID_LOCK_FD)
-    {
+bool unlock_file(LOCK_FD fd) {
+    if (fd == INVALID_LOCK_FD) {
         return true;
     }
 #ifdef _WIN32
     bool ret = CloseHandle(fd);
-    if (ret == false)
-    {
+    if (ret == false) {
         DWORD err = GetLastError();
-		zc_error("unlock file error : %d ", err);
+        zc_error("unlock file error : %d ", err);
     }
 #else
     bool ret = close(fd) == 0;
-    if (ret == false)
-    {
-		zc_error("unlock file error : %s ", strerror(errno));
+    if (ret == false) {
+        zc_error("unlock file error : %s ", strerror(errno));
     }
 #endif
     return ret;
