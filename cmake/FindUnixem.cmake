6ac76d32443346f080c0a541380bb5580a81b748
Tu Duong Quyet <tuduongquyet@gmail.com>
2024-04-11 13:06:53 +0700

    Refactoring CMake, md files

--- cmake/FindUnixem.cmake
+++ cmake/FindUnixem.cmake
@@ -4,24 +4,23 @@
 if (NOT UNIXEM_FOUND)
     if (NOT UNIXEM_INCLUDE_DIR)
         find_path(UNIXEM_INCLUDE_DIR
-            NAMES
+                NAMES
                 unixem/unixem.h
-            ONLY_CMAKE_FIND_ROOT_PATH
+                ONLY_CMAKE_FIND_ROOT_PATH
         )
-    endif()
-    
+    endif ()
+
     if (NOT UNIXEM_LIBRARY)
         find_library(UNIXEM_LIBRARY
-            NAMES unixem 
-            ONLY_CMAKE_FIND_ROOT_PATH
+                NAMES unixem
+                ONLY_CMAKE_FIND_ROOT_PATH
         )
     endif (NOT UNIXEM_LIBRARY)
-    
+
     message(STATUS " UNIXEM_INCLUDE_DIR = ${UNIXEM_INCLUDE_DIR}")
     message(STATUS " UNIXEM_LIBRARY = ${UNIXEM_LIBRARY}")
-    
+
     if (UNIXEM_INCLUDE_DIR AND UNIXEM_LIBRARY)
         set(UNIXEM_FOUND TRUE)
     endif (UNIXEM_INCLUDE_DIR AND UNIXEM_LIBRARY)
-endif()
-    
+endif ()
