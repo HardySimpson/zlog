0d631bbc1aaaee1833a2b30d81b45e31ea236a9a
sheeaza <winsdenny@outlook.com>
2025-12-03 20:37:15 +0800

    test: replace test_pipe with pipe2
    test_pipe use cronolog for test, some os does not have this, disable
    cmake test for test_pipe, add pipe2 using cat for pipe test
    
    Signed-off-by: sheeaza <winsdenny@outlook.com>

---
+++ test/test_pipe2.c
@@ -0,0 +1,40 @@
+/* Copyright (c) Hardy Simpson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "zlog.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int rc;
+    zlog_category_t *zc;
+
+    rc = zlog_init("test_pipe2.conf");
+    if (rc) {
+        printf("init failed\n");
+        return -1;
+    }
+
+    zc = zlog_get_category("my_cat");
+    if (!zc) {
+        printf("get cat fail\n");
+        zlog_fini();
+        return -2;
+    }
+
+    zlog_info(zc, "hello, zlog");
+    zlog_fini();
+    return 0;
+}
